d163e6ac459c2c56d5963f99675de3b7ec26d661
Ammar Qammaz <ammarkov@gmail.com>
2026-03-22 22:21:51 +0200

    ...

--- pic_select.pas
+++ pic_select.pas
@@ -224,7 +224,7 @@ end;
 
 function GUI_PicProperties(x,y:integer):integer;
 const
-  prepare_menu: array[1..5] of ShortString = (
+  prepare_menu: array[1..5] of string = (
     '’νοιγμα καρτέλας εικόνας',
     'Αλλαγή στοιχείων εικόνας',
     'Ορισμός ως φωτογραφία ασθενούς',
